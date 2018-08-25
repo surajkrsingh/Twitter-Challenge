3b29ff3c7c9c5934cafb4e78eb1e3572f058455a
Suraj singh <suraj.sk243@gmail.com>
2018-08-25 18:59:49 +0530

    Update prepareTweetsBundle.php

--- prepareTweetsBundle.php
+++ prepareTweetsBundle.php
@@ -1,4 +1,15 @@
 <?php
+**
+ * @version 1.0
+ * @package  Twitter Challenge
+ * @category PHP
+ * @author   Suraj kumar Singh <spsrga@gmail.com>
+ * @since    24-08-2018
+ * @link     https://surajkrsingh.000webhostapp.com/Twitter/
+ *
+ * Parepare the tweets csv file to upload on drive.
+ *
+ **/
 session_start();
 error_reporting(0);
 include_once("includes/config.php");
